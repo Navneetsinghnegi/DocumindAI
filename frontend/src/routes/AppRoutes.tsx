368ec082bdf9d5a8e4cef268a48bc23fea7ce22b
Navneetsinghnegi <navneetsingh5647@gmail.com>
2026-01-29 03:16:06 +0530

    feat: added welcome page

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -2,13 +2,14 @@ import { Route,Routes,Navigate } from "react-router-dom"
 import Auth from "../pages/Auth"
 import Dashboard from "../pages/Dashboard"
 import ProtectedRoute from "./ProtectedRoute"
+import Welcome from "../pages/Welcome"
 
 const AppRoutes = () => {
   return (
     <Routes>
 
-        <Route path="/" element={<Navigate to="/auth" replace/>}></Route>
-        
+        <Route path="/" element={<Navigate to="/welcome" replace/>}></Route>
+        <Route path="/welcome" element={<Welcome/>}/>
         <Route path="/auth" element={<Auth/>}></Route>
         <Route path="/dashboard" element={
           <ProtectedRoute>
